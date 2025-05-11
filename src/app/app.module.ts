4e76c6a9416d7f80b303b0cdfb9cc3186f25f553
sanmish4 <sanmish4@icloud.com>
2025-05-11 18:43:52 -0400

    changes for live stock

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent // Declaring the component
+  ],
+  imports: [FormsModule]
+})
+
+export class AppModule { }
