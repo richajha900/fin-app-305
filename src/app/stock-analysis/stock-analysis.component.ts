f8697ad42b87fbd8efa8b86494dc4c3d0100e544
sanmish4 <sanmish4@icloud.com>
2025-05-14 00:37:40 -0400

    cache memory handle

--- src/app/stock-analysis/stock-analysis.component.ts
+++ src/app/stock-analysis/stock-analysis.component.ts
@@ -4,6 +4,25 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/internal/operators/catchError';
 import { throwError } from 'rxjs/internal/observable/throwError';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheService {
+  get(key: string): any | null {
+    const cached = localStorage.getItem(key);
+    return cached ? JSON.parse(cached) : null;
+  }
+
+  set(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+}
 
 @Component({
   selector: 'app-stock-analysis',
@@ -19,7 +38,7 @@ export class StockAnalysisComponent {
   //country: string = ''
  // countries: string [] = ['India','USA','Canada'];
   //state: string[] = ['Maharashtra','NC','ON'];
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cacheService: CacheService) { }
   isPositive(value: string): boolean {
     const numericValue = parseFloat(value.replace('%', '').trim());
     return numericValue > 0;
@@ -39,67 +58,88 @@ export class StockAnalysisComponent {
     else
     {
       this.errorMessage = '';
-      const apiKey = 'MEO8NKHWH1GUDQKL'; // Replace with your API key
-      const fetchUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${this.selectedStock}&apikey=${apiKey}`;
-      const overviewUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${this.selectedStock}&apikey=${apiKey}`;
+      const stockCacheKey = `stock-${this.selectedStock}`;
+      const overviewCacheKey = `overview-${this.selectedStock}`;
+
+      // Try to get cached data
+      const cachedStock = this.cacheService.get(stockCacheKey);
+      const cachedOverview = this.cacheService.get(overviewCacheKey);
+
+      if (cachedStock) {
+        this.selectedStockDet = cachedStock;
+      }
+      if (cachedOverview) {
+        this.selectedStockOverview = cachedOverview;
+      }
+      if (cachedStock && cachedOverview) {
+        console.log('Using cached data:', cachedStock, cachedOverview);
+        return; // Use cached data, skip API calls
+      }
+      else {
+        const apiKey = 'MEO8NKHWH1GUDQKL'; // Replace with your API key
+        const fetchUrl = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${this.selectedStock}&apikey=${apiKey}`;
+        const overviewUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${this.selectedStock}&apikey=${apiKey}`;
 
 
-      this.http.get(fetchUrl)
-        .pipe(
-          catchError(this.handleError)
-        )
-        .subscribe({
-          next: (data: any) => {
-            if (data['Global Quote'] && data['Global Quote']['05. price']) {
-              console.log('Stock data:', data);
-              this.selectedStockDet = {
-                symbol: data['Global Quote']['01. symbol'],
-                open: data['Global Quote']['02. open'],
-                high: data['Global Quote']['03. high'],
-                low: data['Global Quote']['04. low'],
-                price: data['Global Quote']['05. price'],
-                volume: data['Global Quote']['06. volume'],
-                latestTradingDay: data['Global Quote']['07. latest trading day'],
-                previousClose: data['Global Quote']['08. previous close'],
-                change: data['Global Quote']['09. change'],
-                changePercent: data['Global Quote']['10. change percent']
-              };
-            } else {
-              this.errorMessage = 'No data found for the given stock symbol.';
-              console.log(this.errorMessage);
+        this.http.get(fetchUrl)
+          .pipe(
+            catchError(this.handleError)
+          )
+          .subscribe({
+            next: (data: any) => {
+              if (data['Global Quote'] && data['Global Quote']['05. price']) {
+                console.log('Stock data:', data);
+                this.selectedStockDet = {
+                  symbol: data['Global Quote']['01. symbol'],
+                  open: data['Global Quote']['02. open'],
+                  high: data['Global Quote']['03. high'],
+                  low: data['Global Quote']['04. low'],
+                  price: data['Global Quote']['05. price'],
+                  volume: data['Global Quote']['06. volume'],
+                  latestTradingDay: data['Global Quote']['07. latest trading day'],
+                  previousClose: data['Global Quote']['08. previous close'],
+                  change: data['Global Quote']['09. change'],
+                  changePercent: data['Global Quote']['10. change percent']
+                };
+                this.cacheService.set(stockCacheKey, this.selectedStockDet);
+              } else {
+                this.errorMessage = 'No data found for the given stock symbol.';
+                console.log(this.errorMessage);
+              }
+            },
+            error: (error) => {
+              console.error('Error fetching stock data:', error);
+              this.errorMessage = 'Failed to fetch stock data. Please try again.';
             }
-          },
-          error: (error) => {
-            console.error('Error fetching stock data:', error);
-            this.errorMessage = 'Failed to fetch stock data. Please try again.';
-          }
-        });
-      // Fetch Overview data
-      this.http.get(overviewUrl)
-        .pipe(
-          catchError(this.handleError)
-        )
-        .subscribe({
-          next: (data: any) => {
-            if (data && data['PERatio']) {
-              console.log('Overview data:', data);
-              // Merge Overview data into selectedStockDet
-              this.selectedStockOverview = {
-                marketCap: data['MarketCapitalization'],
-                name: data['Name'] ,
-                peRatio: data['PERatio'],
-                dividendYield: data['DividendYield']
-              };
-            } else {
-              this.errorMessage = 'No data found for the given stock symbol in Overview.';
-              console.log(this.errorMessage);
+          });
+        // Fetch Overview data
+        this.http.get(overviewUrl)
+          .pipe(
+            catchError(this.handleError)
+          )
+          .subscribe({
+            next: (data: any) => {
+              if (data && data['PERatio']) {
+                console.log('Overview data:', data);
+                // Merge Overview data into selectedStockDet
+                this.selectedStockOverview = {
+                  marketCap: data['MarketCapitalization'],
+                  name: data['Name'],
+                  peRatio: data['PERatio'],
+                  dividendYield: data['DividendYield']
+                };
+                this.cacheService.set(overviewCacheKey, this.selectedStockOverview);
+              } else {
+                this.errorMessage = 'No data found for the given stock symbol in Overview.';
+                console.log(this.errorMessage);
+              }
+            },
+            error: (error) => {
+              console.error('Error fetching Overview data:', error);
+              this.errorMessage = 'Failed to fetch Overview data. Please try again.';
             }
-          },
-          error: (error) => {
-            console.error('Error fetching Overview data:', error);
-            this.errorMessage = 'Failed to fetch Overview data. Please try again.';
-          }
-        });
+          });
+      }
     }
   }
 
